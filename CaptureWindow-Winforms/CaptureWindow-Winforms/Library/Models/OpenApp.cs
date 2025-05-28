488efbc9a4e510482f77a3f48c90570f5f8e139c
emman-j <176351546+emman-j@users.noreply.github.com>
2025-05-28 16:23:32 +0800

    add model for openapps

---
+++ CaptureWindow-Winforms/CaptureWindow-Winforms/Library/Models/OpenApp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptureWindow_Winforms.Library.Models
+{
+    public class OpenApp
+    {
+        public IntPtr Handle { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString() => Title;
+    }
+}
