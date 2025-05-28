936d98299db21dfb9b71ea0744da49497600747c
emman-j <176351546+emman-j@users.noreply.github.com>
2025-05-28 16:24:44 +0800

    added client class to separate view from code

---
+++ CaptureWindow-Winforms/CaptureWindow-Winforms/Library/Client.cs
@@ -0,0 +1,70 @@
+﻿using CaptureWindow_Winforms.Forms;
+using CaptureWindow_Winforms.Library.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureWindow_Winforms.Library
+{
+    public class Client
+    {
+        internal TabControl tabControl;
+        internal TabManager tabManager;
+        internal WindowManager windowManager;
+
+        public Client(TabControl tabControl)
+        {
+            windowManager = new WindowManager();
+            tabManager = new TabManager(tabControl, windowManager);
+            this.tabControl = tabControl;
+        }
+
+        public void Close()
+        {
+            windowManager.CleanUp();
+            windowManager.CloseAllEmbeddedApps();
+            Application.Exit();
+        }
+
+        public void FormResized()
+        {
+            windowManager.ResizeAndDockApp(tabManager.selectedTab);
+        }
+
+        public void TitleBarMouseDown(Control control)
+        {
+            Form parentForm = control.Parent.FindForm();
+            if (parentForm != null)
+            {
+                windowManager.ReleaseCapture();
+                windowManager.SendMessage(parentForm.Handle, 0x112, 0xf012, 0);
+            }
+        }
+
+        public void LauchAndDock(Form form)
+        { 
+            windowManager.LaunchAndDockApp(form);
+        }
+
+        public void SelectOpenApp()
+        {
+            using (OpenAppSelection_Form selectionForm = new OpenAppSelection_Form(this))
+            {
+                if (selectionForm.ShowDialog() == DialogResult.OK)
+                {
+                    IntPtr selectedAppHandle = selectionForm.SelectedApp.Handle;
+                    string selectedAppTitle = selectionForm.SelectedApp.Title;
+
+                    if (selectedAppHandle != IntPtr.Zero && tabManager.selectedTab != null)
+                    {
+                        windowManager.EmbedSelectedApp(selectedAppHandle, tabManager.selectedTab);
+                        tabManager.selectedTab.Text = selectedAppTitle;
+                    }
+                }
+            }
+        }
+    }
+}
