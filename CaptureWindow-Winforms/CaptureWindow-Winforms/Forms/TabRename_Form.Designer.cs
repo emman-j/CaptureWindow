2dab903ebefb01b24ee1319391945b45266ea2ca
emman-j <176351546+emman-j@users.noreply.github.com>
2025-05-29 08:34:12 +0800

    Added rename dialogue form

---
+++ CaptureWindow-Winforms/CaptureWindow-Winforms/Forms/TabRename_Form.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CaptureWindow_Winforms.Forms
+{
+    partial class TabRename_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            cancelButton = new Button();
+            saveButton = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoSize = true;
+            panel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            panel1.Controls.Add(cancelButton);
+            panel1.Controls.Add(saveButton);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 43);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(447, 29);
+            panel1.TabIndex = 5;
+            // 
+            // cancelButton
+            // 
+            cancelButton.Anchor = AnchorStyles.Right;
+            cancelButton.Location = new Point(335, 2);
+            cancelButton.Margin = new Padding(4, 3, 4, 3);
+            cancelButton.Name = "cancelButton";
+            cancelButton.Size = new Size(52, 24);
+            cancelButton.TabIndex = 1;
+            cancelButton.Text = "Cancel";
+            cancelButton.UseVisualStyleBackColor = true;
+            cancelButton.Click += cancelButton_Click;
+            // 
+            // saveButton
+            // 
+            saveButton.Anchor = AnchorStyles.Right;
+            saveButton.Location = new Point(392, 2);
+            saveButton.Margin = new Padding(4, 3, 4, 3);
+            saveButton.Name = "saveButton";
+            saveButton.Size = new Size(52, 24);
+            saveButton.TabIndex = 0;
+            saveButton.Text = "Save";
+            saveButton.UseVisualStyleBackColor = true;
+            saveButton.Click += saveButton_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Bottom;
+            textBox1.Location = new Point(0, 20);
+            textBox1.Margin = new Padding(4, 3, 4, 15);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(447, 23);
+            textBox1.TabIndex = 4;
+            textBox1.WordWrap = false;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Left;
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 0);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Padding = new Padding(0, 3, 0, 3);
+            label1.Size = new Size(60, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Tab Name";
+            // 
+            // TabRename_Form
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(447, 72);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Name = "TabRename_Form";
+            Text = "Change Tab Name";
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button cancelButton;
+        private Button saveButton;
+        private TextBox textBox1;
+        private Label label1;
+    }
+}
---
+++ CaptureWindow-Winforms/CaptureWindow-Winforms/Forms/TabRename_Form.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureWindow_Winforms.Forms
+{
+    public partial class TabRename_Form : Form, INotifyPropertyChanged
+    {
+        private string _tabName;
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public string TabName
+        { 
+            get => _tabName;
+            set 
+            {
+                if (value != _tabName)
+                { 
+                    _tabName = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyname = "")
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+
+        public TabRename_Form()
+        {
+            InitializeComponent();
+            textBox1.DataBindings.Add("Text", this, nameof(TabName), false, DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            TabName = textBox1.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+        private void Form_ChangeTabName_Load(object sender, EventArgs e)
+        {
+            if (TabName != null)
+            {
+                textBox1.Text = TabName;
+            }
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                saveButton.PerformClick();
+            }
+        }
+    }
+}
