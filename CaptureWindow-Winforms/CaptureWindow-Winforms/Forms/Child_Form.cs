f9a16ce268b8951f1559cb18d908060090ce8b48
emman-j <176351546+emman-j@users.noreply.github.com>
2025-05-29 11:46:31 +0800

    re-added child form for window docking

---
+++ CaptureWindow-Winforms/CaptureWindow-Winforms/Forms/Child_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureWindow_Winforms.Forms
+{
+    public partial class Child_Form : Form
+    {
+        public Child_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
